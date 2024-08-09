3fb6018f13738e38c5109631af9935f0deda8713
MartinsSmirnovs <37780997+MartinsSmirnovs@users.noreply.github.com>
2024-08-09 13:49:11 +0000

    Added Slave json field

---
+++ src/rdbus/config/Slave.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Register.hpp"
+#include <chrono>
+#include <list>
+#include <nlohmann/json.hpp>
+#include <string>
+
+namespace config
+{
+
+struct Slave
+{
+    std::string name;
+    int address = 0;
+
+    using Millis = std::chrono::milliseconds;
+    Millis pollTimeMs = Millis( 0 );
+
+    using Registers = std::list< Register >;
+    Registers registers;
+};
+
+void from_json( const nlohmann::json& j, Slave& x );
+
+} // namespace config
