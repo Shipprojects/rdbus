a9ca601de9928300a8d16b14c48a9ae0a7ae0fb7
MartinsSmirnovs <37780997+MartinsSmirnovs@users.noreply.github.com>
2024-08-10 11:30:45 +0000

    Adjusted comment

--- src/rdbus/communication/modbus/RequestPlanner.hpp
+++ src/rdbus/communication/modbus/RequestPlanner.hpp
@@ -13,7 +13,7 @@ namespace modbus
 // Due to the possibility in configuration to specify registers that are not directly
 // adjacent to each other, we have to execute multiple read requests to read all
 // necessary values. This limitation comes from Modbus protocol itself, where
-// reading happens from x address for n values.
+// reading happens from x address for n registers.
 std::list< MB::ModbusRequest > requestPlan( const config::Slave& slave );
 
 } // namespace modbus
