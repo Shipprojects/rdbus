13821f80c6dde953c931e5a965912b071e664696
MartinsSmirnovs <37780997+MartinsSmirnovs@users.noreply.github.com>
2024-08-21 14:21:55 +0000

    Adde NMEA response parser and it's tests

---
+++ src/rdbus/communication/nmea/Response.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdexcept>
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+namespace rdbus::communication::nmea
+{
+
+// A basic representation of uninterpreted NMEA response
+class Response
+{
+public:
+    Response( const std::vector< uint8_t >& data, bool validateChecksum = false );
+
+    struct Exception : public std::runtime_error
+    {
+        Exception( const std::string& what );
+    };
+
+    const std::string& getTalkerID() const;
+    const std::string& getSentenceID() const;
+
+    using Fields = std::vector< std::string >;
+    const Fields& getFields() const;
+
+private:
+    std::string talkerID;
+    std::string sentenceId;
+
+    Fields fields;
+};
+
+} // namespace rdbus::communication::nmea
