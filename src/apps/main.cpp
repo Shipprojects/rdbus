b991f5af36471f4e4ecff3d2d48da6a8bcdf7495
MartinsSmirnovs <37780997+MartinsSmirnovs@users.noreply.github.com>
2024-08-22 14:34:34 +0000

    Improved exception handling for threads

--- src/apps/main.cpp
+++ src/apps/main.cpp
@@ -7,6 +7,12 @@
 #include <spdlog/spdlog.h>
 #include <thread>
 
+// The return value of the program. Atomic because it is shared between
+// threads so it would be possible to set result to 1 in case of thread
+// failure.
+static volatile std::atomic< int > result = 0;
+
+// The variable which allows 'infinite' loops. Shared between threads.
 static volatile std::atomic< bool > keepRunning = true;
 void signalHandler( int signum )
 {
@@ -17,7 +23,7 @@ void signalHandler( int signum )
     }
 }
 
-void joinThreads( std::list< std::thread >& threads )
+void stop( std::list< std::thread >& threads )
 {
     // Wait for threads to finish
     for ( auto& thread : threads )
@@ -36,7 +42,7 @@ void yield()
     }
 }
 
-void startThreads( std::list< std::thread >& threads, std::list< rdbus::Manager >& managers )
+void start( std::list< std::thread >& threads, std::list< rdbus::Manager >& managers )
 {
     for ( auto& manager : managers )
     {
@@ -46,7 +52,26 @@ void startThreads( std::list< std::thread >& threads, std::list< rdbus::Manager 
             SPDLOG_INFO( "Starting manager of " + manager.getName() );
             while ( keepRunning )
             {
-                manager.run();
+                // This try-catch clause is meant to catch exceptions on actual runtime.
+                // In general most of the exceptions must be handled by manager, however
+                // if some kind of exception comes from it then it means that some critical
+                // error occurred and the program shall exit.
+                try
+                {
+                    manager.run();
+                }
+                catch ( const std::exception& e )
+                {
+                    SPDLOG_CRITICAL( e.what() );
+                    keepRunning = false;
+                    result = 1;
+                }
+                catch ( ... )
+                {
+                    SPDLOG_CRITICAL( "Unknown exception occurred!" );
+                    keepRunning = false;
+                    result = 1;
+                }
                 std::this_thread::sleep_for( std::chrono::milliseconds( 10 ) );
             }
         } );
@@ -57,6 +82,11 @@ int main( int argc, char** argv )
 {
     signal( SIGINT, &signalHandler );
 
+    std::list< std::thread > threads;
+    std::list< rdbus::Manager > managers;
+
+    // This try-catch clause is meant to catch exceptions on configuration
+    // time. In case of invalid configuration the program shall exit.
     try
     {
         const auto& args = parseArguments( argc, argv );
@@ -65,26 +95,27 @@ int main( int argc, char** argv )
         const auto& configs = rdbus::initializeConfigs( args.configDir );
 
         auto output = rdbus::initializeOutput( args.output );
-        auto managers = rdbus::initializeManagers( configs, output );
-
-        std::list< std::thread > threads;
-        startThreads( threads, managers );
-
-        yield();
-
-        joinThreads( threads );
+        managers = rdbus::initializeManagers( configs, output );
     }
     catch ( const std::exception& e )
     {
         SPDLOG_CRITICAL( e.what() );
-        return 1;
+        result = 1;
     }
     catch ( ... )
     {
         SPDLOG_CRITICAL( "Unknown exception occurred!" );
-        return 1;
+        result = 1;
     }
 
+    // Start threads if everything is OK
+    if ( result == 0 )
+    {
+        start( threads, managers );
+        yield();
+    }
+    stop( threads );
+
     SPDLOG_INFO( "Exiting" );
-    return 0;
+    return result;
 }
--- src/rdbus/out/pipe/Pipe.cpp
+++ src/rdbus/out/pipe/Pipe.cpp
@@ -10,11 +10,13 @@ void Pipe::send( const rdbus::Data& data )
 {
     const nlohmann::json j = data;
     constexpr auto indent = 4;
-    std::cout << j.dump( indent ) << std::endl;
+    std::cout << j.dump( indent ) + '\n';
 }
 
 void Pipe::send( const std::list< rdbus::Data >& list )
 {
+    // This is thread safe operation - the only common resource
+    // that gets accessed is std::cout and it is also thread safe
     for ( const auto& data : list )
     {
         send( data );
