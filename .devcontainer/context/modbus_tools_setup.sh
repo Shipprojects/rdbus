daf68d454b761410b2e0d325d0109a414d1074a3
MartinsSmirnovs <37780997+MartinsSmirnovs@users.noreply.github.com>
2024-08-27 19:33:07 +0300

    Added modbus server and client testing apps

---
+++ .devcontainer/context/modbus_tools_setup.sh
@@ -0,0 +1,18 @@
+set -e
+
+git clone https://github.com/serhmarch/ModbusTools.git
+cd ModbusTools
+git submodule update --init --recursive
+
+mkdir build
+cd build
+cmake ..
+make -j10
+
+cd ../..
+mv ModbusTools/build /opt/modbus_tools
+chown --recursive developer:developer /opt/modbus_tools
+rm -rf ModbusTools
+
+ln --symbolic /opt/modbus_tools/server /usr/bin/mbserver
+ln --symbolic /opt/modbus_tools/client /usr/bin/mbclient
