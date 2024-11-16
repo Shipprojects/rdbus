4e8cc48c8c56b041e1fbc75d845972eaf5145839
MartinsSmirnovs <37780997+MartinsSmirnovs@users.noreply.github.com>
2024-11-17 00:53:19 +0200

    Made limits::Processing generic

--- src/rdbus/processing/limits/wago/Processor.hpp
+++ src/rdbus/processing/limits/wago/Processor.hpp
@@ -2,7 +2,6 @@
 
 #include "rdbus/Data.hpp"
 #include "rdbus/config/processors/Limits.hpp"
-#include "rdbus/config/wago/Module.hpp"
 #include "rdbus/processing/Processor.hpp"
 #include "rdbus/processing/limits/Data.hpp"
 #include <chrono>
@@ -19,16 +18,16 @@ namespace rdbus::processing::limits::wago
 class Processor : public processing::Processor
 {
 public:
-    Processor( const std::list< config::wago::Module >&, const config::processors::Limits& );
+    Processor( const config::processors::Limits& );
     Processor::OutputList process( const std::list< rdbus::Data >& ) override;
 
 private:
-    using ModuleName = std::string;
-    using InstanceName = std::string;
+    using DeviceName = std::string;
+    using FieldName = std::string;
     using Timepoint = std::chrono::time_point< std::chrono::system_clock >;
     using TimestampedValue = std::pair< Timepoint, rdbus::Data::Field::Variant >;
-    using TimestampedInstanceValues = std::list< TimestampedValue >;
-    std::map< ModuleName, std::map< InstanceName, TimestampedInstanceValues > > data;
+    using TimestampedFieldValues = std::list< TimestampedValue >;
+    std::map< DeviceName, std::map< FieldName, TimestampedFieldValues > > data;
 
     // This function is a bit of a hack to not to bring std time functions into this class
     // for the sake of simplicity. This function takes the timestamp of the first field
@@ -37,9 +36,10 @@ private:
     // single reading will have time deviations of at most some milliseconds.
     Timepoint getLatestTime( const rdbus::Data& );
 
-    void removeOldValues( const ModuleName& module, const Timepoint& currentTime );
-    void insertNewValues( const ModuleName& module, const std::list< rdbus::Data::Field >& fields );
-    std::shared_ptr< limits::Data > generateOutput( const ModuleName& module );
+    void removeOldValues( const DeviceName& module, const Timepoint& currentTime );
+    void addMissingFields( const DeviceName& module, const std::list< rdbus::Data::Field >& fields );
+    void insertNewValues( const DeviceName& module, const std::list< rdbus::Data::Field >& fields );
+    std::shared_ptr< limits::Data > generateOutput( const DeviceName& module );
 
     // How long to store the values for
     const std::chrono::minutes duration;
